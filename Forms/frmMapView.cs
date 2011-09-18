32553372254d2b3122cfe724e5d205975de8ef77
tryllerot <tryllerot@96a12ea9-52fb-775e-05fb-3fc9d54e31b7>
2011-09-18 03:23:39 +0000

    * Added map view

---
+++ Forms/frmMapView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Tibia;
+using Tibia.Constants;
+using Tibia.Objects;
+using Tibia.Packets;
+using Tibia.Util;
+
+namespace Drakenbot.Forms
+{
+    public partial class frmMapView : Form
+    {
+        Client Client;
+        Player Player;
+        public frmMapView()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMapView_Load(object sender, EventArgs e)
+        {
+            Client = Client.GetClients()[0];
+            Player = Client.GetPlayer();
+        }
+
+        private void t_map_view_Tick(object sender, EventArgs e)
+        {
+            uxMapView.LoadMap();
+            System.Threading.Thread.Sleep(350);
+            uxMapView.SetLevel((Player.Location.Z));
+            uxMapView.SetMapCenter(Player.Location);
+        }
+
+        private void uxZoomIn_Click(object sender, EventArgs e)
+        {
+            uxMapView.Zoom(2.00);
+        }
+
+        private void uxZoomOut_Click(object sender, EventArgs e)
+        {
+            uxMapView.Zoom(0.50);
+        }
+
+        private void uxEnableMapView_CheckedChanged(object sender, EventArgs e)
+        {
+            if (uxEnableMapView.Checked == true)
+            {
+                t_map_view.Start();
+                uxMapView.Markers.Add(new MapViewer.MapMarker(Player));
+            }
+            else
+            {
+                t_map_view.Stop();
+            }
+        }
+    }
+}
